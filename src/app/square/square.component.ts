d32c5a907d6721d5aed74991e022d620a3698557
Hung Dinh <dinhh23@gmail.com>
2021-10-05 17:53:50 -0400

    fixed board font

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,13 +3,13 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <button>{{ value }}</button>
+  <button>{{ value }}</button>
   `,
-  styles: []
+  styles: ['button { width: 100%; height: 100%; font-size: 10em !important; }']
 })
 export class SquareComponent {
 
-  @Input()
-  value: 'X' | 'O' = "X";
+  @Input() value: 'X' | 'O';
 
 }
+
