7b11d09a63d7afbf41483bdba1acd0d6195ad32a
Hung Dinh <dinhhcoding@gmail.com>
2021-10-11 18:07:26 -0400

    fixed start new game button

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -5,7 +5,7 @@ import { Component, Input } from '@angular/core';
   template: `
   <button nbButton *ngIf="!value">{{ value }}</button>
   <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
-  <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+  <button nbButton hero status="warning" *ngIf="value == 'O'">{{ value }}</button>
   
   `,
   styles: ['button { width: 100%; height: 100%; font-size: 10em !important; }']
